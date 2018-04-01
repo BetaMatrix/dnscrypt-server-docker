d47e70c19ba76c9b45c3e4de73a821286334cee8
BetaMatrix <command@sent.com>
2018-04-02 00:17:27 +0800

    Update dnscrypt-wrapper.sh

--- dnscrypt-wrapper.sh
+++ dnscrypt-wrapper.sh
@@ -56,7 +56,7 @@ prune
 
 exec /opt/dnscrypt-wrapper/sbin/dnscrypt-wrapper \
     --user=_dnscrypt-wrapper \
-    --listen-address=0.0.0.0:443 \
+    --listen-address=0.0.0.0:4443 \
     --resolver-address=127.0.0.1:553 \
     --provider-name="$provider_name" \
     --provider-cert-file="$(stcerts_files)" \
